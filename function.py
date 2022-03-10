bc31bf9e9868b0064a36e4dd901b7e7144434da7
poknjuy <1378336803@qq.com>
2022-03-10 15:53:53 +0800

    3.10

--- function.py
+++ function.py
@@ -54,13 +54,14 @@ class rec(object):
             return None
 
     def searchline(self,imgs,img):#霍夫线变换 canny图 
-        edges = cv.Canny(img, 50, 150)
-        cv.imshow('edges', edges)
-        lines = cv.HoughLines(edges, 1, np.pi/180, 300)
+        # edges = cv.Canny(img, 50, 150)
+        # cv.imshow('edges', edges)
+        lines = cv.HoughLines(img, 1, np.pi/180, 300)
         i=300
         while(lines==None):
-            lines = cv.HoughLines(edges, 1, np.pi/180, i) #参数对图3 图4效果均不错
+            lines = cv.HoughLines(img, 1, np.pi/180, i) #参数对图3 图4效果均不错
             i=i-1
+            if (i < 0): return None
             if(lines is None): continue
             else: break
         if(lines.size != 0):
@@ -83,21 +84,30 @@ class rec(object):
             return None
     
     def searchlinepro(self,imgs,img):
-        edges = cv.Canny(img, 50, 150)
+        # edges = cv.Canny(img, 50, 150)
         # cv.imshow('', edges)
-        lines = cv.HoughLinesP(edges, 1, np.pi/180, 200, minLineLength=50)
+        # img = cv.Canny(img, 50, 150)
+        lines = cv.HoughLinesP(img, 1, np.pi/180, 200, minLineLength=img.shape[0]//10)
         # for line in lines:
         #     cv.line(imgs, (line[0][0],line[0][1]), (line[0][2],line[0][3]), (0,0,255),2)
         i=200
+        minlength = img.shape[0]//10
         while(lines is None):
-            lines = cv.HoughLinesP(edges, 1, np.pi/180, i, minLineLength=0) #参数对图3 图4效果均不错
+            lines = cv.HoughLinesP(img, 1, np.pi/180, i, minLineLength=minlength) #参数对图3 图4效果均不错
             i=i-1
+            if (i < 0):
+                minlength = minlength // 2
+                i = 200
+                continue
+            if (minlength <= 10):
+                return rec.searchline(self, imgs, img)
+                
             if(lines is None): continue
             else: break
-            if (i < 0):
-                return None
+            
         if (lines.size != 0):
-            midx = midy = img.shape[0]//2
+            midy = img.shape[0]//2
+            midx = img.shape[1]//2
             if (((lines[0][0][2] - midx) ^ 2 + (lines[0][0][3] - midy) ^ 2) > ((lines[0][0][0] - midx) ^ 2 + (lines[0][0][1] - midy) ^ 2)):
                 cv.line(imgs, (midx, midy), (lines[0][0][2], lines[0][0][3]), (0,0,255), 2)
             else:
@@ -110,5 +120,92 @@ class rec(object):
 
     def searchoutline(self,imgs,img):
         edges = cv.Canny(img, 50, 150)
+        edgescopy = cv.Canny(img, 50, 150)
+        # cv.imshow('', edges)
+        edges = cv.dilate(edges, cv.getStructuringElement(cv.MORPH_RECT, (1, 1)), 1)
+        
         contours, hierarchy = cv.findContours(edges, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         cv.drawContours(imgs, contours, -1, (0,255,0), 1)
+        
+        n = len(contours)
+        cv_contours = []
+        for contour in contours:
+            area = cv.contourArea(contour)
+            if area <= 60:
+                cv_contours.append(contour)
+            else:
+                continue
+        cv.fillPoly(edges, cv_contours, (255, 0, 0))
+        cv.drawContours(edges, cv_contours, -1, (0,255,0), 1)
+        edges = cv.erode(edges, cv.getStructuringElement(cv.MORPH_RECT, (1, 1)), 5)
+        # edges = cv.ximgproc.thinning(edges, THINNING_ZHANGSUEN)
+        # edges = rec.searchline(self, edges, edges)
+        cv.imshow('return', edgescopy)
+        edges = rec.searchminmax(self, edges)
+        cv.imshow('test', edges)
+        
+
+        return edgescopy
+
+    def searchminmax(self, img):
+        
+        img = cv.dilate(img, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), 5)
+        img = cv.erode(img, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), 9)
+        cv.imshow('origin', img)
+        # while(lines==None):
+        #     lines = cv.HoughLines(img, 1, np.pi/180, i) #参数对图3 图4效果均不错
+        #     i=i-1
+        #     if (i < 0): return None
+        #     if(lines is None): continue
+        #     else: break
+        white = np.zeros((img.shape[1], img.shape[0], 3), np.uint8)
+        white.fill(255)
+        # edges = cv.Canny(img, 50, 150)
+        # cv.imshow('', edges)
+        # img = cv.Canny(img, 50, 150)
+        lines = cv.HoughLinesP(img, 1, np.pi/180, 10, minLineLength=30)
+        # for line in lines:
+        #     cv.line(imgs, (line[0][0],line[0][1]), (line[0][2],line[0][3]), (0,0,255),2)
+        # i=200
+        # minlength = img.shape[0]//10
+        # while(lines is None):
+        #     lines = cv.HoughLinesP(img, 1, np.pi/180, i, minLineLength=minlength) #参数对图3 图4效果均不错
+        #     i=i-1
+        #     if (i < 0):
+        #         minlength = minlength // 2
+        #         i = 200
+        #         continue
+        #     if (minlength <= 10):
+        #         return rec.searchline(self, img, img)
+                
+        #     if(lines is None): continue
+        #     else: break
+            
+        if (lines is not None):
+            for line in lines:
+                cv.line(white, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (0,0,255), 2)
+                # cv.imshow('white', white)
+                midy = img.shape[0]//2
+                midx = img.shape[1]//2
+                x1 = line[0][0]
+                x2 = line[0][2]
+                y1 = line[0][1]
+                y2 = line[0][2]
+                A = y2 - y1
+                B = x1 - x2
+                C = x2 * y1 - x1 * y2
+                d = (abs(A * midx + B * midy + C)) / (pow((A * A + B * B), 0.5))
+                if (d <= 99999):
+                    if (((line[0][2] - midx) ^ 2 + (line[0][3] - midy) ^ 2) > ((line[0][0] - midx) ^ 2 + (line[0][1] - midy) ^ 2)):
+                        print()
+                        # cv.line(white, (midx, midy), (line[0][2], line[0][3]), (0,0,255), 2)
+                    else: 
+                        print()
+                        # cv.line(white, (midx, midy), (line[0][0], line[0][1]), (0,0,255), 2)
+                    # cv.line(imgs, (lines[0][0][0], lines[0][0][1]), (lines[0][0][2], lines[0][0][3]), (0,0,255), 2)
+                else: continue
+                # cv.imshow('white', white)
+            cv.imshow('white', white)
+            return img
+        else:
+            return None
--- main.py
+++ main.py
@@ -2,22 +2,41 @@ import cv2 as cv
 
 from function import rec
 
-file = './train/train2/146.jpg'
-img = cv.imread(file)
-imggray = rec().gray(img)
-imgbinary = rec().binary(imggray)
-# cv.imshow('imgbinary', imgbinary)
-test = rec().searchoutline(img, imggray)
-imgc = rec().searchcircle(img, imggray)
-if(imgc is not None):
-    imgcl = rec().searchlinepro(imgc, imgc)
-    if(imgcl is not None):
-        cv.imshow('main', imgcl)
-        cv.waitKey()
-    else:
-        print('false no line')
-else:
-    print('false no circle')
+train1size = 149
+train2size = 180
+for num in range(1, 2):
+    with open('./train/train2/data.txt', "r") as f:
+        # for data in f.readlines():
+            # data = data.strip('\n')
+            data = 6.30
+            print(data)
+            file = './train/train2/' + str(num) + '.jpg'
+            img = cv.imread(file)
+            if (img is not None) :
+                imggray = rec().gray(img)
+                imgbinary = rec().binary(imggray)
+                # cv.imshow('imgbinary', imgbinary)
+                
+                imgc = rec().searchcircle(img, imggray)
+                
+                if(imgc is not None):
+                    test = rec().searchoutline(imgc, imgc)
+                    imgcl = rec().searchlinepro(imgc, test)
+                    if(imgcl is not None):
+                        print('good')
+                        cv.imshow('main' + str(num), imgcl)
+                        # cv.waitKey()
+                    else:
+                        print('error:' + str(num) + ' false no line')
+                        # cv.waitKey()
+                else:
+                    print('error:' + str(num) + ' false no circle')
+                    # cv.waitKey()
+                # exit()
+            else:
+                print('read file error')
+cv.waitKey()
+
 # rec().searchline(imggray)
 
 # cv.imwrite('G:/recognition/result.jpg', img)
